189f0078faaf1d09506101e2f13c7b7c4202d9fe
Edwin Garcia Quiterio <edwinegq@outlook.com>
2025-10-19 22:22:52 -0600

    REFACTOR: Validar formulario de empleado.

--- src/features/employee/infraestructure/hooks/useEmployeeForm.ts
+++ src/features/employee/infraestructure/hooks/useEmployeeForm.ts
@@ -1,21 +1,87 @@
 'use client';
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useEmployeeUIStore } from "../stores/employee-ui.store";
 import * as yup from 'yup';
+import { useForm } from "react-hook-form";
 
 export const schema = yup.object().shape({
-    
+    employeeRoleId: yup.string()
+        .required('Debes elegir el rol para el empleado.')
+        .test('not-empty', 'Debes elegir la categoría del producto.', 
+            value => value !== undefined && value !== null && value !== ''),
+    firstName: yup.string()
+        .required('El nombre es obligatorio.')
+        .min(3, 'El nombre debe tener mínimo 3 caracteres.')
+        .max(100, 'El nombre debe tener máximo 100 caracteres.')
+        .typeError('Asegurate de ingresar la información correcta.'),
+    lastName: yup.string()
+        .required('Los apellidos son obligatorios.')
+        .min(3, 'Los apellidos debe tener mínimo 3 caracteres.')
+        .max(100, 'Los apellidos debe tener máximo 100 caracteres.')
+        .typeError('Asegurate de ingresar la información correcta.'),
+    email: yup.string()
+        .email('El formato para el correo es alberto@platform.com')
+        .optional()
+        .notRequired()
+        .default('email@virtual.com')
+        .typeError('Asegurate de ingresar la información correcta.'),
+    phoneNumber: yup.string()
+        .required('El número de teléfono es obligatorios.')
+        .max(20, 'El número de teléfono debe tener máximo 20 caracteres.')
+        .typeError('Asegurate de ingresar la información correcta.'),
+    birthDate: yup.date()
+        .transform((value, originalValue) => originalValue === '' ? null : value)
+        .optional().notRequired().nullable(),
+    gender: yup.string()
+        .nullable()
+        .optional()
+        .notRequired(),
+    hireDate: yup.date()
+        .transform((value, originalValue) => originalValue === '' ? null : value)
+        .optional().notRequired().nullable(),
+    currentSalary: yup.number()
+        .nullable()
+        .optional()
+        .notRequired()
+        .typeError('Asegurate de ingresar la información correcta.'),
+    entryTime: yup.string()
+        .transform((value, originalValue) => originalValue === '' ? null : value)
+        .optional().notRequired().nullable(),
+    exitTime: yup.string()
+        .transform((value, originalValue) => originalValue === '' ? null : value)
+        .optional().notRequired().nullable(),
 });
+
+type FormData = yup.InferType<typeof schema>;
+
 const useEmployeeForm = () => {
-    const { setAddressStateCheck, setUserStateCheck } = useEmployeeUIStore();
+    const { setAddressStateCheck, setUserStateCheck, addressStateCheck, userStateCheck } = useEmployeeUIStore();
     const handleUserCheck = (e: React.ChangeEvent<HTMLInputElement>)=>{
         setUserStateCheck(!!e.target.checked.valueOf());
     }
     const handleAddressCheck = (e: React.ChangeEvent<HTMLInputElement>)=>{
         setAddressStateCheck(!!e.target.checked);
     }
+
+    const { register, handleSubmit, reset, setValue, watch, clearErrors, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+        defaultValues: {
+        }
+    });
+
+    const onSubmit = async (data: FormData) => {
+        console.log(data)
+    }
     return {
+        addressStateCheck, 
+        userStateCheck,
         handleAddressCheck,
-        handleUserCheck
+        handleUserCheck,
+        onSubmit,
+        handleSubmit,
+        register,
+        errors,
     }
 }
 
--- src/features/employee/ui/register/EmployeeForms.tsx
+++ src/features/employee/ui/register/EmployeeForms.tsx
@@ -7,6 +7,8 @@ import clsx from 'clsx';
 import { EmployeeFormAddress } from './EmployeeFormAddress';
 import { EmployeeFormUser } from './EmployeeFormUser';
 import { GenderEnum } from '../../domain/enums/gender.enum';
+import { useEmployeeForm } from '../../infraestructure/hooks/useEmployeeForm';
+import { Button } from '@/ui/components/buttons';
 
 interface Props {
     optionsRoles: SelectMenuOption[]
@@ -15,62 +17,108 @@ interface Props {
 const EmployeeForms = ({ optionsRoles }: Props) => {
     const genderOptions = Object.values(GenderEnum).map(item => ({text: item.toUpperCase(), value: item}));
 
-    const { addressStateCheck, userStateCheck} = useEmployeeUIStore()
+    const { addressStateCheck, userStateCheck, errors, onSubmit, register, handleSubmit} = useEmployeeForm()
     return (
         <div className="flex gap-4 w-full">
             <div className="w-full">
                 <h1 className="text-blue-600 font-semibold bg-blue-100 rounded-t-2xl p-2 text-lg uppercase text-center">Información del empleado</h1>
-                <div className="bg-white p-4 rounded-b-2xl">
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-4 rounded-b-2xl">
                     <div>
                         <LabelInput value="Nombre" required="yes" />
-                        <TextInput placeholder="Introduce el nombre" />
+                        <TextInput 
+                            {...register('firstName')}
+                            type='text'
+                            error={!!errors.firstName}
+                            errorMessage={errors.firstName?.message}
+                            name="firstName"
+                            placeholder="Ej: Roberto" />
                     </div>
                     <div>
                         <LabelInput value="Apellidos" required="yes" />
-                        <TextInput placeholder="Introduce los apellidos" />
+                        <TextInput 
+                            {...register('lastName')}
+                            type='text'
+                            error={!!errors.lastName}
+                            errorMessage={errors.lastName?.message}
+                            name="lastName"
+                            placeholder="Ej: Bustos Quiterio" />
                     </div>
                     <div>
                         <LabelInput value="Típo de empleado" required="yes" />
-                        <SelectMenu items={optionsRoles} />
+                        <SelectMenu 
+                            {...register('employeeRoleId')}
+                            error={!!errors.employeeRoleId}
+                            errorMessage={errors.employeeRoleId?.message}
+                            name="employeeRoleId"
+                            items={optionsRoles} />
                     </div>
                     <div>
                         <LabelInput value="Número de teléfono" required="yes" />
                         <TextInput
+                            {...register('phoneNumber')}
                             type="tel"
-                            placeholder="Introduce no de teléfono" />
+                            error={!!errors.phoneNumber}
+                            errorMessage={errors.phoneNumber?.message}
+                            name="phoneNumber"
+                            placeholder="Ej: 7417682345" />
                     </div>
                     <div>
                         <LabelInput value="Correo" required="no" />
                         <TextInput
+                            {...register('email')}
                             type="email"
-                            placeholder="Introduce un correo" />
+                            error={!!errors.email}
+                            errorMessage={errors.email?.message}
+                            name="email"
+                            placeholder="Ej: roberto@email.com" />
                     </div>
                     <div>
                         <LabelInput value="Fécha de nacimiento" required="no" />
                         <TextInput
                             type="date"
+                            {...register('birthDate')}
+                            error={!!errors.birthDate}
+                            errorMessage={errors.birthDate?.message}
+                            name="birthDate"
                             placeholder="Fecha de nacimiento" />
                     </div>
                     <div>
                         <LabelInput value="Genero" required="no" />
-                        <SelectMenu items={genderOptions} />
+                        <SelectMenu 
+                            {...register('gender')}
+                            error={!!errors.gender}
+                            errorMessage={errors.gender?.message}
+                            name="gender"
+                            items={genderOptions} />
                     </div>
                     <div>
                         <LabelInput value="Fécha de contratación" required="no" />
                         <TextInput
                             type="date"
+                            {...register('hireDate')}
+                            error={!!errors.hireDate}
+                            errorMessage={errors.hireDate?.message}
+                            name="hireDate"
                             placeholder="Selecciona" />
                     </div>
                     <div>
                         <LabelInput value="Hora de entrada" required="no" />
                         <TextInput
                             type="time"
+                            {...register('entryTime')}
+                            error={!!errors.entryTime}
+                            errorMessage={errors.entryTime?.message}
+                            name="entryTime"
                             placeholder="Selecciona" />
                     </div>
                     <div>
                         <LabelInput value="Hora de salida" required="no" />
                         <TextInput
                             type="time"
+                            {...register('exitTime')}
+                            error={!!errors.exitTime}
+                            errorMessage={errors.exitTime?.message}
+                            name="exitTime"
                             placeholder="Selecciona" />
                     </div>
                     <div>
@@ -78,9 +126,16 @@ const EmployeeForms = ({ optionsRoles }: Props) => {
                         <TextInput
                             min={0.0}
                             type="number"
-                            placeholder="Selecciona" />
+                            {...register('currentSalary')}
+                            error={!!errors.currentSalary}
+                            errorMessage={errors.currentSalary?.message}
+                            name="currentSalary"
+                            placeholder="Ej: 200" />
                     </div>
-                </div>
+                    <Button type='submit'>
+                        Guardar
+                    </Button>
+                </form>
             </div>
             <div className={clsx(`${(!userStateCheck && !addressStateCheck) && 'hidden'} w-full flex flex-col gap-4`)}>
                 {userStateCheck && <>
