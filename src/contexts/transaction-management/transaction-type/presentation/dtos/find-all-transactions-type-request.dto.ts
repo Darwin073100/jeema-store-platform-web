682ef5e7c1344e6a16baa5fc107937e4d520cf58
Edwin Garcia Quiterio <edwinegq@outlook.com>
2026-03-13 16:34:54 -0600

    REFACTOR: Refactorizacion de todos los contextos.

--- src/contexts/transaction-management/transaction-type/presentation/dtos/find-all-transactions-type-request.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsNotEmpty } from "class-validator";
-import { AccountTypeEnum } from "../../domain/enums/account-type.enum";
-
-export class FindAllTransactionsTypeRequestDTO {
-    @IsEnum(AccountTypeEnum, {message: `El típo de movimiento debe ser (${Object.create(AccountTypeEnum)})`})
-    @IsNotEmpty({message: 'El típo de movimiento es obligatorio'})
-    accountType: AccountTypeEnum;
-}
